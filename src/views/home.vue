1e4122b3c1e423ff3ac14909ae453e517a21139e
gary-the-gamer <gary.the.gamer42069@gmail.com>
2022-10-07 22:16:33 +0300

    check out and more sutff in the home page

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,5 +1,17 @@
 <template>
 <div>
+  <v-carousel  :show-arrows="true" 
+class="mx-auto"
+     
+      max-width="1864"
+      position="absolute"
+      height="1000 " >
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+    ></v-carousel-item>
+  </v-carousel>
   <v-container>
   <v-hover v-slot="{ hover }">
     <v-card
@@ -18,7 +30,7 @@
             class="d-flex transition-fast-in-fast-out black v-card--reveal text-h2 white--text"
             style="height: 100%;"
           >
-            $10000.00
+            $1800.00
           </div>
         </v-expand-transition>
       </v-img>
@@ -27,7 +39,7 @@
         style="position: relative;"
       >
         <v-label>
-          <a href="">BUY NOW</a>
+          <v-btn src="">BUY</v-btn>
         </v-label>
         <div class="font-weight-light grey--text text-h6 mb-2">
           for the best gaming experience
@@ -167,18 +179,50 @@
     </v-card>
   </v-hover>
   </v-container> 
-<v-carousel  :show-arrows="true" 
-class="mx-auto"
-     
+  <v-container>
+  <v-hover v-slot="{ hover }">
+    <v-card
+    class="mx-auto"
+      color="grey lighten-4"
       max-width="1864"
       position="absolute"
-      height="1000 " >
-    <v-carousel-item
-      v-for="(item,i) in items"
-      :key="i"
-      :src="item.src"
-    ></v-carousel-item>
-  </v-carousel>
+    >
+      <v-img
+        :aspect-ratio="16/9"
+        src="71YC16-yKPL.jpg"
+      >
+        <v-expand-transition>
+          <div
+            v-if="hover"
+            class="d-flex transition-fast-in-fast-out green darken-2 v-card--reveal text-h2 white--text"
+            style="height: 100%;"
+          >
+            $1200.00
+          </div>
+        </v-expand-transition>
+      </v-img>
+      <v-card-text
+        class="pt-6"
+        style="position: relative;"
+      >
+        <div class="font-weight-light grey--text text-h6 mb-2">
+         <v-btn>BUY</v-btn>
+          <br>
+          Up Your Creative Game
+        </div>
+        <h3 class="text-h4 font-weight-light green--text mb-2">
+          8K HDR Gaming
+        </h3>
+        <div class="font-weight-light text-h6 mb-2">
+          Connect, play, capture, and watch in brilliant HDR at resolutions 
+          up to 8K with GeForce RTX 3090 Ti or RTX 3090. Capture up to 8K
+           HDR footage with the GeForce Experience™ ShadowPlay™ feature 
+           and play back smoothly with AV1 decode.
+        </div>
+      </v-card-text>
+    </v-card>
+  </v-hover>
+</v-container>
 </div>
 </template>
 
@@ -207,10 +251,10 @@ class="mx-auto"
             src: 'dlss301.jpg',
           },
           {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+            src: 'shop-2080-ti-1070@2x.jpg',
           },
           {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+            src: 'maxresdefault1.jpg',
           },
         ],
       }
