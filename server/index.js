b33939d892f3a3ae32ca7b750e09309857df3454
gary-the-gamer <gary.the.gamer42069@gmail.com>
2022-11-02 19:38:23 +0300

    123

--- server/index.js
+++ server/index.js
@@ -1,12 +1,73 @@
-const express = require('express');
-const mysql = require('mysql');
+import express  from 'express';
+import  mysql  from'mysql';
+import bodyParser from 'body-parser';
 
 
 
 
 const app = express()
+const port = process.env.PORT || 5000
 
+app.use(bodyParser.urlencoded({extended:false}))
 
-app.listen("3000", () => {
-    console.log(`server is live`)
+app.use(bodyParser.json())
+
+//MySQL code
+const pool = mysql.createPool({
+    connectionLimit : 10,
+    host            :'localhost',
+    user            : 'root',
+    password        : '',
+    database        :'ice group store'
 })
+
+
+//Get acounts
+app.get('/', (req,res) =>{
+    pool.getConnection((err,connection)=>{
+        if(err) throw err
+        console.log(`connected as id ${connection.threadId}`)
+
+
+        connection.query('SELECT * from accounts', (err,rows)=>{
+            connection.release() // return the connection to pool
+            if(!err){
+                res.send(rows)
+            }else{
+                console.log(err)
+            }
+        })
+    })
+})
+
+
+
+
+
+
+app.post('/', (req,res) =>{
+    pool.getConnection((err,connection)=>{
+        if(err) throw err
+        console.log(`connected as id ${connection.threadId}`)
+
+        const params = req.body
+        connection.query('INSERT INTO accounts SET ?', params, (err,rows)=>{
+            connection.release() // return the connection to pool
+            if(!err){
+                res.send(`Account with the Record id:${params.email} has benn added`)
+            }else{
+                console.log(err)
+            }
+        })
+        console.log
+    })
+})
+
+
+
+
+
+
+
+/// listen on envirement port
+app.listen(port, () =>console.log(`sever is live and on port ${port}`))
--- src/views/sign_up.vue
+++ src/views/sign_up.vue
@@ -4,37 +4,10 @@
         <v-row>
           <v-col
             cols="12"
-            md="5"
-          >
-            <v-text-field
-              v-model="firstname"
-              :rules="nameRules"
-              :counter="10"
-              label="First name"
-              required
-            ></v-text-field>
-          </v-col>
-  
-          <v-col
-            cols="12"
-            md="5"
-          >
-            <v-text-field
-              v-model="lastname"
-              :rules="nameRules"
-              :counter="10"
-              label="Last name"
-              required
-            ></v-text-field>
-          </v-col>
-  
-          <v-col
-            cols="12"
             md="8"
           >
             <v-text-field
               v-model="email"
-              :rules="emailRules"
               label="E-mail"
               required
 
@@ -48,7 +21,6 @@
           type="password"
       v-model="password"
       :counter="20"
-      :rules="passwordRules"
       label="password"
       required
     ></v-text-field>
@@ -59,9 +31,8 @@
           >
           <v-text-field
           type="password"
-      v-model="password"
+      v-model="confirmPassword"
       :counter="20"
-      :rules="confirmPasswordRules"
       label="confirm password"
       required
     ></v-text-field>
@@ -83,35 +54,13 @@
  <script>
 import axios from 'axios'
 export default ({
-
-
-
-     valid: false,
-     firstname: '',
-     lastname: '',
-     nameRules: [
-       v => !!v || 'Name is required',
-       v => v.length <= 10 || 'Name must be less than 10 characters',
-     ],
-     
-     email: '',
-     emailRules: [
-       v => !!v || 'E-mail is required',
-       v => /.+@.+/.test(v) || 'E-mail must be valid',
-     ],
-     password: '',
-     
-      passwordRules: [
-        v => !!v || 'password is required',
-        v => (v && v.length >= 6 ) || 'password must be more than 6 characters',
-        v => (v && v.length <= 20 ) || 'password must be less than 20 characters',
-      ],
-      confirmPasswordRules: [
-        v => !!v || 'password is required',
-        v => (v && v.length >= 6 ) || 'password must be more than 6 characters',
-        v => (v && v.length <= 20 ) || 'password must be less than 20 characters',
-       
-      ],
+  data(){
+        return{
+        email: '',
+        password: '',
+        }
+      },
+   
 
       submitData:function(){
    if(application.email != '' && application.password != '')
